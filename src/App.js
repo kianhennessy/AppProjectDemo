8708366d33d6373e573693db59bb9d5e9703f9db
kianhennessy <66495302+kianhennessy@users.noreply.github.com>
2022-12-11 17:10:48 +0000

    Updated to include firebase project details

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-
+    apiKey: "AIzaSyCxKo6uQS37dj5wXkJ-UVmTai64d7F7_9E",
+    authDomain: "messageappdemo-cd24a.firebaseapp.com",
+    projectId: "messageappdemo-cd24a",
+    storageBucket: "messageappdemo-cd24a.appspot.com",
+    messagingSenderId: "737910771326",
+    appId: "1:737910771326:web:b91aaf09db923a0cad522f",
+    measurementId: "G-DR81J2XCXD"
 })
 
 const auth = firebase.auth();
